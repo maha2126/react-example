1b84ab2ec13ad37200604db669fbd78a46a7779d
maha2126 <mariam.petrosyan1998@mail.ru>
2023-03-07 18:45:43 +0400

    react example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +21,31 @@ function App() {
       </header>
     </div>
   );
+} */
+
+class App extends React.Component{
+  state = {num:0}
+
+  /* countUp = () => {
+    this.setState(current =>{
+      current.num++
+      return current
+    })
+  } */
+  countUp = () =>{
+    this.setState(current =>{
+      return {
+        num: current.num+1
+      }
+    })
+  }
+  render(){
+    const {num} = this.state
+    return<div>
+      <h1>number-{num}</h1>
+      <button onClick={this.countUp}>Up</button>
+    </div>
+  }
 }
 
 export default App;
